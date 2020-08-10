63c4d0b7512fdcba55dfd2b3c8a569e9b175c565
zeemai <zee.hao.mai@gmail.com>
2020-08-10 15:52:46 +0100

    started with react navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Navbar from './components/Navbar'
+
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <div class="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+        </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
