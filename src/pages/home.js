9ed3856e893a1f37f5b0344827ca0e7b1a66178a
zeemai <zee.hao.mai@gmail.com>
2020-08-12 23:01:43 +0100

    Added firebase end point/Card style and details

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
 
-export class home extends Component {
+import Scream from '../components/Scream';
+
+class home extends Component {
+    state = {
+        screams: null
+    }
+
+    componentDidMount(){
+        axios
+            .get('/screams')
+            .then(res => {
+                console.log({res})
+                console.log(res.data)
+                this.setState({
+                    screams: res.data
+                });
+            })
+            .catch(err => console.log(err))
+    }
+
     render() {
+        let recentScreamsMarkup = this.state.screams ? (
+            this.state.screams.map((scream) => <Scream scream={scream} />)
+            //this.state.screams.map(scream => <p>{scream.body}</p>)
+        ) : <p>Loading...</p>
+
         return (
-            <div>
-                <h1>Home page</h1>
-            </div>
+            <Grid container spacing={4} >
+                <Grid item sm={8} xs={12}>
+                    {recentScreamsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile</p>
+                </Grid>
+            </Grid>
         )
     }
 }
