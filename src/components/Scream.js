7c1ebda1ca5787f972c6b76961035845d58925fa
zeemai <zee.hao.mai@gmail.com>
2020-08-13 21:16:24 +0100

    Login Form & styling

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -23,6 +26,9 @@ const styles = {
 
 class Scream extends Component {
     render() {
+
+        dayjs.extend(relativeTime);
+
         const { classes, 
                 scream : { 
                     body,
@@ -42,7 +48,7 @@ class Scream extends Component {
                 />
                 <CardContent className={classes.content}>
                     <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
-                    <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+                    <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                     <Typography variant="body1">{body}</Typography> 
                     <Typography variant="body1">{likeCount} Likes</Typography>                                         
                 </CardContent>
