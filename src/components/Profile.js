9a7e32017fddc852dcd4f537594e1f0e775e3b08
zeemai <zee.hao.mai@gmail.com>
2020-08-20 22:33:50 +0100

    impletment upload image function

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,7 +11,10 @@ import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
 import dayjs from 'dayjs';
 import Paper from '@material-ui/core/Paper';
-
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import Tooltip from '@material-ui/core/Tooltip';
+import { logoutUser, uploadImage } from '../redux/actions/userActions';
 
 const styles = {
     paper: {
@@ -62,6 +65,20 @@ const styles = {
 }
 
 class Profile extends Component {
+
+    handleImageChange = (event) => {
+        const image = event.target.files[0];
+        // send this pic to server
+        const formData = new FormData();
+        formData.append('image', image, image.name);
+        this.props.uploadImage(formData);
+    }
+
+    handleEditPicture = () => {
+        const fileInput = document.getElementById('imageInput');
+        fileInput.click();
+    }
+
     render() {
         const { classes, 
                 user: { credentials: {handle, createdAt, bio, imageUrl, location, website }, 
@@ -79,6 +96,12 @@ class Profile extends Component {
                 <div className={classes.profile}>
                     <div className="image-wrapper">
                         <img src={imageUrl} alt="profile" className="profile-image"/>
+                        <input type="file" id="imageInput" hidden="hidden" onChange={this.handleImageChange} />
+                        <Tooltip title="Edit profile picture" placement="top">
+                            <IconButton onClick={this.handleEditPicture} className="button">
+                                <EditIcon color="primary"/>
+                            </IconButton>                            
+                        </Tooltip>
                     </div>
                     <hr />
                     <div className="profile-details">
@@ -145,9 +168,16 @@ const mapStateToProps = (state) =>({
     user: state.user
 })
 
+const mapActionsToProps = {
+    logoutUser,
+    uploadImage
+}
+
 Profile.propTypes = {
     user: PropTypes.object.isRequired,
-    classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    logoutUser: PropTypes.func.isRequired,
+    uploadImage: PropTypes.func.isRequired
   };
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile));
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile));
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -65,3 +65,12 @@ export const getUserData = () => (dispatch) => {
         })
         .catch(err => console.log(err))
 }
+
+export const uploadImage = (formData) => (dispatch) => {
+    dispatch({ type: LOADING_USER});
+    axios.post('/user/image', formData)
+        .then(() => {
+            dispatch(getUserData())
+        })
+        .catch(err => console.log(err))
+}
